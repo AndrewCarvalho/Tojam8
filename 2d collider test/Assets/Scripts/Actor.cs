5e69ef393d02885be8b356c8579a29e4e9539b12
Frankie Leung <frankieleung1@hotmail.com>
2013-05-05 17:42:12 -0400

    blocks

--- 2d collider test/Assets/Scripts/Actor.cs
+++ 2d collider test/Assets/Scripts/Actor.cs
@@ -115,6 +115,7 @@ public class Actor : MonoBehaviour {
 
     protected void Run(float right, bool crouched = false, bool switchAnimations = true)
     {
+        Debug.Log("right " + right + " crouched " + crouched);
         if (right == 0.0f)
         {
             this.runState = RUN_STATE_TEMP.STATIONARY;
